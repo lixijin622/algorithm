d8bb93b57ed378a39ad3158fe503903930a2a534
x <lixijin622@foxmail.com>
2016-04-13 11:16:55 +0800

    test

--- src/Fundamentals/BasicProgramModel/isEqual.java
+++ src/Fundamentals/BasicProgramModel/isEqual.java
@@ -4,4 +4,19 @@ package Fundamentals.BasicProgramModel;
  * Created by x on 2016/4/13.
  */
 public class isEqual {
+    public static boolean isEqual(int a,int b,int c){
+        if(a==b&&b==c){
+            return true;
+        }else {
+            return  false;
+        }
+    }
+
+    public static void main(String[] args){
+        if(isEqual(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]))){
+            System.out.print("equal");
+        }else{
+            System.out.print("not equal");
+        }
+    }
 }
