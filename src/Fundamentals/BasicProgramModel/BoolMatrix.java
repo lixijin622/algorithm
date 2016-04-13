8057b7e85018e18e6d28e80f179c579d811bd935
x <lixijin622@foxmail.com>
2016-04-13 17:31:30 +0800

    toBinaryString

--- src/Fundamentals/BasicProgramModel/BoolMatrix.java
+++ src/Fundamentals/BasicProgramModel/BoolMatrix.java
@@ -6,15 +6,20 @@ package Fundamentals.BasicProgramModel;
 public class BoolMatrix {
     public static void main(String[] args){
         boolean[][] bMatrix={{true,false,false,true},{true,false,false}};
+        System.out.print("  ");
         for(int i=0;i<bMatrix.length;i++){
-            System.out.println(i);
-            for(int j=0;j<bMatrix[i].length;j++){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        for(int i=0;i<bMatrix[i].length;i++){
+            for(int j=0;j<bMatrix.length;j++){
                 System.out.print(j+" ");
-                if(bMatrix[i][j]){
+                if(bMatrix[j][i]){
                 System.out.print("* ");
                 }else {
                     System.out.print("  ");
                 }
+                System.out.println("");
             }
         }
     }
